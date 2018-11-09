31cd48f1f859b6c2d7146c0beb4a199a2b039eef
Daniel Savoiu <daniel.savoiu@cern.ch>
2018-11-09 17:52:11 +0100

    [PostProcessing] Rename `pp.py` to `lumberjack.py`, which is a better name for a program that cuts down TTrees :)

--- PostProcessing/scripts/lumberjack_tasks.sh
+++ PostProcessing/scripts/lumberjack_tasks.sh
@@ -1,11 +1,11 @@
 INFILE_MC="/ceph/dsavoiu/Dijet/results/DijetAna_QCD_RunIISummer16MiniAODv2_2018-10-29/merged.root"
 INFILE_DATA="/ceph/dsavoiu/Dijet/results/DijetAna_JetHT_Run2016G-Legacy-07Aug2017-v1_2018-10-29/merged.root"
-OUTPUT_FILE_SUFFIX="2018-10-31"
+OUTPUT_FILE_SUFFIX="2018-11-08"
 
 echo "Sourcing master ROOT stack... [/cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-slc6-gcc62-opt/setup.sh]..."
 source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-slc6-gcc62-opt/setup.sh
 
-pp.py -i "$INFILE_MC" \
+lumberjack.py -i "$INFILE_MC" \
       --type mc \
       -j10 \
       --log --progress \
@@ -13,7 +13,7 @@ pp.py -i "$INFILE_MC" \
       task JetResponse EventYieldMC Flavors OccupancyMC PFEnergyFractionsMC \
       --output-file-suffix "$OUTPUT_FILE_SUFFIX"
 
-pp.py -i "$INFILE_DATA" \
+lumberjack.py -i "$INFILE_DATA" \
       --type data \
       -j10 \
       --log --progress \
