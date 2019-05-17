3bc34573bd54020894f5c5e8388931947d28437b
Daniel Savoiu <daniel.savoiu@cern.ch>
2019-05-17 19:10:34 +0200

    [Skimming] Refactor Karma skim configs: place in dedicated module `Configuration`.

---
+++ Skimming/python/Configuration/MiniAOD/karmaSkim_94X_cff.py
@@ -0,0 +1,353 @@
+"""
+Karma skim config for 94X miniAOD files
+=======================================
+
+This skim config should be used for 2016 samples produced with CMSSW 94X,
+e.g. 17Jul2018 Re-MiniAOD or the corresponding Monte-Carlo.
+
+To use:
+
+    # import the module
+    from Karma.Skimming.Configuration.MiniAOD import karmaSkim_94X_cff
+
+    # register the options
+    options = karmaSkim_94X_cff.register_options(options)
+
+    # configure the process
+    karmaSkim_94X_cff.configure(process)
+
+
+"""
+import FWCore.ParameterSet.Config as cms
+
+
+def register_options(options):
+    """Command-Line option flags used by the configuration below."""
+    return (
+        options
+            .register('jsonFilterFile',
+                      type_=str,
+                      default=None,
+                      description="Path to JSON file containing certified runs and luminosity blocks.")
+            .register('useHLTFilter',
+                      type_=bool,
+                      default=True,
+                      description="If True, only events triggered by a path matching the configured regex will be written out.")
+            .register('withPATCollections',
+                      type_=bool,
+                      default=None,
+                      description="Path to JSON file containing certified runs and luminosity blocks.")
+    )
+
+def configure(process, options):
+    """Apply configuration to a process."""
+
+    # create the main module path
+    process.add_path('path')
+
+    # enable the JSON filter (if given)
+    if options.jsonFilterFile:
+        process.enable_json_lumi_filter(options.jsonFilterFile)
+
+    # == configure CMSSW modules ==========================================
+
+    # -- Jets (default from miniAOD) --------------------------------------
+
+    if options.withPATCollections:
+        # just write out miniAOD jets
+        process.add_output_commands(
+            'keep patJets_slimmedJets_*_*',
+            'keep patJets_slimmedJetsAK8_*_*',
+        )
+
+    # -- Jets (from miniAOD, but with possibly new JECs from GT) ----------
+
+    from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
+
+    updateJetCollection(
+        process,
+        jetSource = cms.InputTag('slimmedJets'),
+        labelName = 'UpdatedJEC',
+        jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual']), 'None')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
+    )
+
+    updateJetCollection(
+        process,
+        jetSource = cms.InputTag('slimmedJetsAK8'),
+        labelName = 'UpdatedJECAK8',
+        jetCorrections = ('AK8PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual']), 'None')  # Update: Safe to always add 'L2L3Residual' as MC contains dummy L2L3Residual corrections (always set to 1)
+    )
+
+    process.jecSequence = cms.Sequence(process.patJetCorrFactorsUpdatedJEC * process.updatedPatJetsUpdatedJEC)
+    process.jecSequenceAK8 = cms.Sequence(process.patJetCorrFactorsUpdatedJECAK8 * process.updatedPatJetsUpdatedJECAK8)
+
+    process.path *= process.jecSequence
+    process.path *= process.jecSequenceAK8
+
+    if options.withPATCollections:
+        process.add_output_commands(
+            'keep patJets_updatedPatJetsUpdatedJEC_*_*',
+            'keep patJets_updatedPatJetsUpdatedJECAK8_*_*',
+        )
+
+    # -- Jets (reclustered with jet toolbox) ------------------------------
+
+    from Karma.Common.Sequences.jetToolbox_cff import addJetToolboxSequences
+
+    # create reclustering sequences
+
+    jet_collection_names = []
+
+    # AK4CHS jets (include pileupJetID)
+    jet_collection_names += addJetToolboxSequences(
+        process, isData=options.isData,
+        min_jet_pt=15,
+        jet_algorithm_specs=('ak4',),
+        pu_subtraction_methods=('CHS',),
+        do_pu_jet_id=True
+    )
+
+    # AK8CHS jets (no pileupJetID available)
+    jet_collection_names += addJetToolboxSequences(
+        process, isData=options.isData,
+        min_jet_pt=15,
+        jet_algorithm_specs=('ak8',),
+        pu_subtraction_methods=('CHS',),
+        do_pu_jet_id=False
+    )
+
+    # AK4Puppi and AK8Puppi jets
+    jet_collection_names += addJetToolboxSequences(
+        process, isData=options.isData,
+        min_jet_pt=15,
+        jet_algorithm_specs=('ak4', 'ak8',),
+        pu_subtraction_methods=('Puppi',),
+        do_pu_jet_id=False
+    )
+
+    # put reclustering sequences on path
+    for _jet_collection_name in jet_collection_names:
+        process.path *= getattr(process, _jet_collection_name)
+        ## write out reclustered jets
+        #process.add_output_commands('keep patJets_{}_*_*'.format(_jet_collection_name))
+
+    # -- Jet ID (precomputed and embedded as userInts) -------------------
+
+    for _jet_collection_name in jet_collection_names:
+        _id_producer_name = "{}IDValueMap".format(_jet_collection_name)
+        _enriched_jet_collection_name = "{}WithJetIDUserData".format(_jet_collection_name)
+
+        # produce the jet id value map
+        setattr(
+            process,
+            _id_producer_name,
+            cms.EDProducer("PatJetIDValueMapProducer",
+                filterParams = cms.PSet(
+                    version = cms.string('WINTER16'),
+                    quality = cms.string('TIGHTLEPVETO'),
+                ),
+                src = cms.InputTag(_jet_collection_name)
+            )
+        )
+
+        # embed jet id information in pat::Jet itprocess
+        setattr(
+            process,
+            _enriched_jet_collection_name,
+            cms.EDProducer("PATJetUserDataEmbedder",
+                 src = cms.InputTag(_jet_collection_name),
+                 userInts = cms.PSet(
+                    tightIdLepVeto = cms.InputTag(_id_producer_name),
+                 ),
+            )
+        )
+
+        # add modules to path
+        process.path *= getattr(process, _id_producer_name)
+        process.path *= getattr(process, _enriched_jet_collection_name)
+
+        # write out ID-enriched jet collection
+        if options.withPATCollections:
+            process.add_output_commands(
+                'keep patJets_{}_*_*'.format(_enriched_jet_collection_name)
+            )
+
+    # -- MET --------------------------------------------------------------
+
+    from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+
+    # run this to keep MET Type-I correction up-to-date with currently applied JECs
+    runMetCorAndUncFromMiniAOD(
+       process,
+       isData=True,
+    )
+
+    process.path *= process.fullPatMetSequence
+
+    if options.withPATCollections:
+        process.add_output_commands(
+            'keep patMETs_slimmedMETs_*_*',
+        )
+
+    # -- Electrons --------------------------------------------------------
+
+    # just write out miniAOD electrons
+    if options.withPATCollections:
+        process.add_output_commands(
+            "keep patElectrons_slimmedElectrons_*_*",
+        )
+
+    # Note: electron scale/smearing correction information is contained in
+    # the following userFloats: 'ecalEnergyPreCorr' and 'ecalEnergyPostCorr'
+
+    # Note: electron ID information is stored as "pseudo-userData" in
+    # PAT::Electrons (not in the inherited PAT::Object userData variables)
+    # and can be accessed using PAT::Electrons::electronID() with the
+    # corresponding tag (e.g. 'cutBasedElectronID-Summer16-80X-V1-loose')
+
+
+    # -- Muons ------------------------------------------------------------
+
+    # just write out miniAOD muons
+    if options.withPATCollections:
+        process.add_output_commands(
+            "keep patMuons_slimmedMuons_*_*",
+        )
+
+    # -- Primary Vertices -------------------------------------------------
+
+    from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+
+    # "good" primary vertices
+
+    process.add_module(
+        'goodOfflinePrimaryVertices',
+        cms.EDFilter(
+            'PrimaryVertexObjectFilter',
+            src = cms.InputTag("offlineSlimmedPrimaryVertices"),
+            filterParams = pvSelector.clone(
+                maxZ = 24.0
+            ),  # ndof >= 4, rho <= 2
+        ),
+        on_path='path',
+        write_out=False,
+    )
+
+    # == END configure CMSSW modules ======================================
+
+
+
+    # == configure Karma modules ==========================================
+
+    # -- General Event Information ----------------------------------------
+
+    from Karma.Skimming.EventProducer_cfi import karmaEventProducer
+
+    process.add_module(
+        'karmaEvents',
+        karmaEventProducer(isData=options.isData).clone(
+            goodPrimaryVerticesSrc = cms.InputTag("goodOfflinePrimaryVertices"),
+        ),
+        on_path='path',
+        write_out=True,
+    )
+
+    # filter out event if no interesting HLT path fired (if requested)
+    if options.useHLTFilter:
+        process.add_module(
+            'karmaEventHLTFilter',
+            cms.EDFilter("EventHLTFilter",
+                cms.PSet(
+                    karmaEventSrc = cms.InputTag("karmaEvents")
+                )
+            ),
+            on_path='path',
+            write_out=False, # don't write out the TriggerResults object
+        )
+
+    # -- Trigger Objects --------------------------------------------------
+
+    from Karma.Skimming.TriggerObjectCollectionProducer_cfi import karmaTriggerObjectCollectionProducer
+
+    process.add_module(
+        'karmaTriggerObjects',
+        karmaTriggerObjectCollectionProducer.clone(
+            karmaRunSrc = cms.InputTag("karmaEvents"),
+        ),
+        on_path='path',
+        write_out=True,
+    )
+
+    # -- MET --------------------------------------------------------------
+
+    from Karma.Skimming.METCollectionProducer_cfi import karmaMETCollectionProducer
+
+    process.add_module(
+        'karmaMETs',
+        karmaMETCollectionProducer.clone(
+            inputCollection = cms.InputTag("slimmedMETs"),
+        ),
+        on_path='path',
+        write_out=True,
+    )
+
+    # -- Jets -------------------------------------------------------------
+
+    from Karma.Skimming.JetCollectionProducer_cfi import karmaJets
+
+    # create "karma::Jet" collections from pat::Jets
+    for _jet_collection_name in jet_collection_names:
+
+        # add karma modules for producing the skimmed jet collections
+        _module_name = "karma{}{}".format(_jet_collection_name[0].upper(), _jet_collection_name[1:])
+        process.add_module(
+            _module_name,
+            karmaJets.clone(
+                inputCollection = cms.InputTag("{}WithJetIDUserData".format(_jet_collection_name)),
+            ),
+            on_path='path',
+            write_out=True,
+        )
+
+    # -- Electrons --------------------------------------------------------
+
+    from Karma.Skimming.ElectronCollectionProducer_cfi import karmaElectronCollectionProducer
+
+    process.add_module(
+        'karmaElectrons',
+        karmaElectronCollectionProducer.clone(),
+        on_path='path',
+        write_out=True,
+    )
+
+    # -- Muons ------------------------------------------------------------
+
+    from Karma.Skimming.MuonCollectionProducer_cfi import karmaMuonCollectionProducer
+
+    process.add_module(
+        'karmaMuons',
+        karmaMuonCollectionProducer.clone(),
+        on_path='path',
+        write_out=True,
+    )
+
+    # -- Primary Vertices -------------------------------------------------
+
+    from Karma.Skimming.VertexCollectionProducer_cfi import karmaVertexCollectionProducer
+
+    process.add_module(
+        'karmaVertices',
+        karmaVertexCollectionProducer.clone(),
+        on_path='path',
+        write_out=True,
+
+    )
+
+    # == END configure Karma modules ======================================
+
+
+    # selective writeout based on path decisions
+    process.enable_selective_writeout('path')
+
+    # just in case we need it
+    return process
--- Skimming/test/FullSkim/testFullSkim_cfg.py
+++ Skimming/test/FullSkim/testFullSkim_cfg.py
@@ -1,131 +1,46 @@
+import os
+
+import FWCore.ParameterSet.Config as cms
 
 from Karma.Common.Tools import KarmaOptions, KarmaProcess
-
-from Karma.Common.Sequences.jetToolbox_cff import addJetToolboxSequences
-from Karma.Common.Sequences.jetEnergyCorrections_cff import undoJetEnergyCorrections
+from Karma.Skimming.Configuration.MiniAOD import karmaSkim_94X_cff
 
 
-# -- override CLI options for test
-#options.inputFiles="file://{}".format(os.path.realpath("../../data/test_JetHT2016G.root"))
-options.inputFiles="file://{}".format("/ceph/storage/c/dsavoiu/miniaod-test/data/test_JetHT2016G.root")
-options.isData=1
-options.globalTag="80X_dataRun2_2016LegacyRepro_v4"
-options.edmOut="testFullSkim_out_1000.root"
-#options.maxEvents=1000
-options.edmOut="testFullSkim_out.root"
-options.maxEvents=-1
-options.dumpPython=1
+# set up and parse command-line options
+options = (
+    karmaSkim_94X_cff.register_options(KarmaOptions())
+        .setDefault('inputFiles', "file:///storage/9/dsavoiu/test_miniAOD/test_DoubleEG_Run2016G_17Jul2018_MINIAOD.root")
+        .setDefault('outputFile', "testFullSkim_out.root")
+        .setDefault('isData', True)
+        .setDefault('globalTag', "94X_dataRun2_v10")
+        .setDefault('maxEvents', 1000)#.setDefault('maxEvents', -1)
+        .setDefault('dumpPython', True)
+        .setDefault('useHLTFilter', False)
+        .setDefault('jsonFilterFile', "")
+        #.setDefault('jsonFilterFile', "{}/src/Karma/Skimming/data/json/2016/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.json".format(os.getenv("CMSSW_BASE")))
+        .setDefault('withPATCollections', False)
+        .setDefault('numThreads', 1)
+).parseArguments()
 
 
-# -- must be called at the beginning
-process = createProcess("DIJET", num_threads=1)
-
-# modules will add to this list to register a product
-# for writeout
-_accumulated_output_commands = ['drop *']
-
-
-# -- only process certified runs and lumisections
-if options.isData:
-    process.source.lumisToProcess = LumiList.LumiList(
-        filename = os.path.realpath("{}/src/Karma/Skimming/data/json/2016/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.json".format(os.getenv("CMSSW_BASE")))
-    ).getVLuminosityBlockRange()
-
-## enable verbose log file output
-#enableVerboseLogging(process)
-
-# ignore errors from HLTPrescaleProvider (issued when
-# no unique L1 prescale can be found for a HLT path)
-process.MessageLogger.categories.append("HLTPrescaleProvider")
-process.MessageLogger.cerr.HLTPrescaleProvider = cms.untracked.PSet(
-    limit = cms.untracked.int32(2),
+# create the process
+process = KarmaProcess(
+    "KARMASKIM",
+    input_files=options.inputFiles,
+    max_events=options.maxEvents,
+    global_tag=options.globalTag,
+    edm_out=options.outputFile,
+    num_threads=options.numThreads,
 )
 
-# -- configure CMSSW modules
+process.enable_verbose_logging()  # for testing
 
-from Karma.Skimming.TriggerObjectCollectionProducer_cfi import karmaTriggerObjectCollectionProducer
-from Karma.Skimming.METCollectionProducer_cfi import karmaPFMETCollectionProducer, karmaCHSMETCollectionProducer
-from Karma.Skimming.JetCollectionProducer_cfi import karmaJets
-from Karma.Skimming.EventProducer_cfi import karmaEventProducer
-from Karma.Skimming.VertexCollectionProducer_cfi import karmaVertexCollectionProducer
+# configure the process
+karmaSkim_94X_cff.configure(process, options)
 
-from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
-process.goodOfflinePrimaryVertices = cms.EDFilter('PrimaryVertexObjectFilter',
-    src = cms.InputTag("offlineSlimmedPrimaryVertices"),
-    filterParams = pvSelector.clone(
-        maxZ = 24.0
-    ),  # ndof >= 4, rho <= 2
-)
+# dump expanded cmsRun configuration
+if options.dumpPython:
+    process.dump_python('.'.join(options.outputFile.split('.')[:-1]) + '_dump.py', overwrite=True)
 
-process.karmaEvents = karmaEventProducer(isData=options.isData).clone(
-    goodPrimaryVerticesSrc = cms.InputTag("goodOfflinePrimaryVertices"),
-)
-_accumulated_output_commands.append("keep *_karmaEvents_*_KARMA")
-
-# filter events for which "interesting" HLT paths fired
-process.karmaEventHLTFilter = cms.EDFilter("EventHLTFilter",
-    cms.PSet(
-        karmaEventSrc = cms.InputTag("karmaEvents")
-    )
-)
-_accumulated_output_commands.append("drop *_karmaEventHLTFilter_*_KARMA")
-
-process.karmaTriggerObjects = karmaTriggerObjectCollectionProducer.clone(
-    karmaRunSrc = cms.InputTag("karmaEvents"),
-)
-_accumulated_output_commands.append("keep *_karmaTriggerObjects_*_KARMA")
-
-process.karmaVertices = karmaVertexCollectionProducer.clone()
-_accumulated_output_commands.append("keep *_karmaVertices_*_KARMA")
-
-mainSequence = cms.Sequence(
-    process.karmaEvents *
-    process.karmaEventHLTFilter *
-    process.karmaTriggerObjects *
-    process.karmaVertices
-);
-
-
-# use JetToolbox to recluster (JEC-uncorrected) jets
-uncorrected_jet_collection_names = addJetToolboxSequences(
-    process,
-    isData=options.isData,
-    jet_algorithm_specs=('ak4', 'ak8'),
-    pu_subtraction_methods=('CHS',),
-    do_pu_jet_id=False)
-
-# create "karma::Jet" collections for JEC-uncorrected pat::Jets
-for _jet_collection_name in uncorrected_jet_collection_names:
-    _module_name = "karma{}{}".format(_jet_collection_name[0].upper(), _jet_collection_name[1:])
-    setattr(
-        process,
-        _module_name,
-        karmaJets.clone(
-            inputCollection = cms.InputTag(_jet_collection_name),
-        )
-    )
-    _accumulated_output_commands.extend([
-        "keep *_{}_*_KARMA".format(_module_name),
-    ])
-    mainSequence *= getattr(process, _jet_collection_name)
-
-# create "karma::MET" collections for (uncorrected) PF and CHS Mets
-process.karmaPFMETs = karmaPFMETCollectionProducer.clone()
-mainSequence *= process.karmaPFMETs
-_accumulated_output_commands.append("keep *_karmaPFMETs_*_KARMA")
-
-process.karmaCHSMETs = karmaCHSMETCollectionProducer(process, isData=options.isData).clone()
-mainSequence *= process.karmaCHSMETs
-_accumulated_output_commands.append("keep *_karmaCHSMETs_*_KARMA")
-
-process.path = cms.Path(mainSequence)
-
-#_accumulated_output_commands.append("keep *_selectedPatTrigger_*_*")
-
-# -- must be called at the end
-finalizeAndRun(process, outputCommands=_accumulated_output_commands)
-
-# selective writeout based on path decisions
-process.edmOut.SelectEvents = cms.untracked.PSet(
-    SelectEvents = cms.vstring('path')
-)
+# print out configuration before running
+process.print_configuration()
