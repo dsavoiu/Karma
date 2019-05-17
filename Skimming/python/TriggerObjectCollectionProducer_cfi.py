5496bb253566d52fda9fdd3e61bfa5cec6199a07
Daniel Savoiu <daniel.savoiu@cern.ch>
2019-05-17 19:18:25 +0200

    [Skimming] Ensure 'cfi' files only contain simple module configurations.

--- Skimming/python/TriggerObjectCollectionProducer_cfi.py
+++ Skimming/python/TriggerObjectCollectionProducer_cfi.py
@@ -1,10 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
+from Karma.Common.util import CMSSW_VERSION
+
+
+TRIGGER_MODULE_NAME = 'selectedPatTrigger'
+if CMSSW_VERSION >= (9,2,1):
+    TRIGGER_MODULE_NAME = 'slimmedPatTrigger'
 
 karmaTriggerObjectCollectionProducer = cms.EDProducer(
     "TriggerObjectCollectionProducer",
     cms.PSet(
-        inputCollection = cms.InputTag("selectedPatTrigger", processName=cms.InputTag.skipCurrentProcess()),
+        inputCollection = cms.InputTag(TRIGGER_MODULE_NAME, processName=cms.InputTag.skipCurrentProcess()),
         karmaRunSrc = cms.InputTag("karmaEvents"),
         triggerResultsSrc = cms.InputTag("TriggerResults", "", "HLT"),
     )
